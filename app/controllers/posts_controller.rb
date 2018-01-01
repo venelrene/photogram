fe4a431553b54178d88f76e15516b27bd4ead696
venelrene <venelrene@gmail.com>
2018-01-01 17:33:31 -0500

    tidy the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,13 +10,11 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.create(post_params)
 
-    if @post.save
+    if @post = Post.create(post_params)
       flash[:success] = "Your post have been created!"
       redirect_to posts_path
     else
@@ -26,12 +25,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       flash[:success] = "Your post have been updated!"
       redirect_to(posts_path(@post))
@@ -42,7 +38,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -54,5 +49,8 @@ class PostsController < ApplicationController
     params.require(:post).permit(:image, :caption)
   end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
 end
