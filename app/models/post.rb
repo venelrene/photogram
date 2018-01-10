297db75a80993b9ac72aba93cae8fc04e088a9ff
venelrene <venelrene@gmail.com>
2018-01-09 19:37:52 -0500

    Comments were added w/AJAX
    A comment model to store comments
    A comment controller to handle the comment actions
    A comment form for the index and show views
    A list of comments for each post so we can see our own and others
    comments too

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,6 +20,7 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { medium: "640x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 end
