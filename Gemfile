7d9efecc71d45d040a0061765a5a4a60cf99d519
venelrene <venelrene@gmail.com>
2018-01-11 23:45:48 -0500

    second test and added factor girl rails

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'factory_bot_rails'
+  gem "factory_girl_rails", "~> 4.0"
+
 end
 
 group :development do
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -16,4 +16,19 @@ feature 'Creating posts' do
     expect(page).to have_content('#coffeetime')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
+
+
+  # visit root route
+  # click the 'New Post' link
+  # fill in the caption field (without touching the image field)
+  # click the 'Create Post' button
+  # expect the page to say, "Halt, you fiend! You need an image to post here!"
+  it 'needs an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'No picture because YOLO'
+    click_button 'Create Post'
+    expect(page).to have_content('Halt, you fiend! You need an image to post here!')
+  end
+
 end
