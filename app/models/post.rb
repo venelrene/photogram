65c5b0f2033649d5dd66f6a0ad54424dadfdd959
venelrene <venelrene@gmail.com>
2018-01-19 22:30:56 -0500

    Added pagination with some Ajax
    Now users can go to the next page instead of endless scrolling.
    Added a circle loading indicator

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,6 +14,10 @@
 #
 
 class Post < ApplicationRecord
+
+  belongs_to :user, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   validates :image, presence: true
   validates :user_id, presence: true
   validates :caption, length: { minimum: 3, maximum: 300 }
@@ -21,7 +25,5 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { medium: "640x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  belongs_to :user, dependent: :destroy
-  has_many :comments, dependent: :destroy
 
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id                 :integer          not null, primary key
+#  caption            :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  user_id            :integer
+#
+
 FactoryBot.define do
   factory :post do
     caption "nofilter"
