c315fbe614eb75d7e4f0121f9e3cb440d541e53f
venelrene <venelrene@gmail.com>
2018-02-04 19:46:36 -0500

    profile create, users can now create & edit their profile data
    Fix the issue causing no method error. Had to do with user name in
    routes
    
    Added avatar to user model
    Added Bio to User table

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-
-  get ':username', to: 'profiles#show', as: :profile
-  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
+
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
     resource :comments
